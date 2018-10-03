2b9100f2d28b0f279ac2d870a3f2d30a1ed0618f
chenshaoming <csming1995@gmail.com>
2018-10-03 18:38:23 +0800

    修改App主色调

---
+++ app/src/main/java/com/csming/dontforgetme/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.csming.dontforgetme.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.csming.dontforgetme.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
