f395681642073210c276e651fef980e8172e7498
chenshaoming <csming1995@gmail.com>
2018-11-18 16:11:00 +0800

    【Feature】撸好了记本发布界面

--- app/src/main/java/com/csming/dontforgetme/book/AddBookActivity.java
+++ app/src/main/java/com/csming/dontforgetme/book/AddBookActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import com.csming.dontforgetme.R;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import dagger.android.support.DaggerAppCompatActivity;
 
 /**
@@ -12,9 +14,40 @@ import dagger.android.support.DaggerAppCompatActivity;
  */
 public class AddBookActivity extends DaggerAppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_book);
+
+        setToolBar();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    private void setToolBar() {
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setTitle(R.string.add_book);
+//            actionBar.setDisplayUseLogoEnabled();
+        }
+
+        mToolbar.setOnMenuItemClickListener(item -> false);
     }
 }
